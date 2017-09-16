342506fb0c0a1cda0d22b077a1f1720cc2ba9e38
Chris McDonald <mail.chris.mcdonald@gmail.com>
2017-09-16 10:37:51 -0600

    division & remainder tests

--- tests/bigint_tests.rs
+++ tests/bigint_tests.rs
@@ -4,6 +4,7 @@ extern crate numerics;
 static MUL_TEST_VECTOR: &str = include_str!("./test_vectors/mul_test.txt");
 static SUB_TEST_VECTOR: &str = include_str!("./test_vectors/sub_test.txt");
 static ADD_TEST_VECTOR: &str = include_str!("./test_vectors/add_test.txt");
+static DIV_TEST_VECTOR: &str = include_str!("./test_vectors/div_test.txt");
 
 #[test]
 fn mul_test_vector_test() {
@@ -59,5 +60,26 @@ fn add_test_vector_test() {
         let b = BigInt::from_str(b).unwrap();
         let c = BigInt::from_str(c).unwrap();
         assert_eq!(a + b, c);
+    }
+}
+
+#[test]
+fn div_test_vector_test() {
+    use numerics::bigint::BigInt;
+    use std::str::FromStr;
+
+    for line in DIV_TEST_VECTOR.lines() {
+        let mut ele = line.split_whitespace();
+        let (x, y, q, r) = (
+            ele.next().unwrap(),
+            ele.next().unwrap(),
+            ele.next().unwrap(),
+            ele.next().unwrap(),
+        );
+        let x = BigInt::from_str(x).unwrap();
+        let y = BigInt::from_str(y).unwrap();
+        let q = BigInt::from_str(q).unwrap();
+        let r = BigInt::from_str(r).unwrap();
+        assert_eq!(x.div_mod(&y), (q, r));
     }
 }
