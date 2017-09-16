a34614eda3d644732f65698fd2d9e9cf0fec904f
Chris McDonald <mail.chris.mcdonald@gmail.com>
2017-09-16 09:43:59 -0600

    here we go to docs land

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(slice_patterns)]
 #![cfg_attr(feature = "bench", feature(test))]
 #![cfg_attr(feature = "thicc_ints", feature(i128_type))]
+#![warn(missing_docs)]
 
 #[macro_use]
 extern crate nom;
